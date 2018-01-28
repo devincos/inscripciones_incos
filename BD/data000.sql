58c6ba2fc5430167df2070134f15affb71b0b909
elialba <elialbarod@gmail.com>
2018-01-28 12:59:34 -0400

    Se agregaron los inserts para el registro de forma de evaluacion

---
+++ BD/data000.sql
@@ -0,0 +1,7 @@
+-------------------------------------I-eli--00-28-01-2018--------------------------------------
+insert into forma_evaluacion(id, evaluacion)values(1,'Anual');
+insert into forma_evaluacion(id, evaluacion)values(2,'Semestral');
+insert into forma_evaluacion(id, evaluacion)values(3,'Trimestral');
+insert into forma_evaluacion(id, evaluacion)values(4,'Bimestral');
+insert into forma_evaluacion(id, evaluacion)values(5,'Mensual');
+-------------------------------------F-eli-00-28-01-2018--------------------------------------
