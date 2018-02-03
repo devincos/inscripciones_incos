ec6c31d4fb429c1fdea8676df3aced0eb23142de
elialba <elialbarod@gmail.com>
2018-02-03 18:06:52 -0400

    se insertaron los datos parmetrizados a FormaEvaluacion y Modalidad

--- BD/data000.sql
+++ BD/data000.sql
@@ -5,3 +5,11 @@ insert into forma_evaluacion(id, evaluacion)values(3,'Trimestral');
 insert into forma_evaluacion(id, evaluacion)values(4,'Bimestral');
 insert into forma_evaluacion(id, evaluacion)values(5,'Mensual');
 -------------------------------------F-eli-00-28-01-2018--------------------------------------
+
+-------------------------------------I-eli--00-03-02-2018--------------------------------------
+INSERT INTO public.modalidad ("id", "modalidad")
+VALUES 
+  (3, E'bimestral'),
+  (2, E'semestral'),
+  (1, E'anual');
+-------------------------------------F-eli-00-03-02-2018--------------------------------------
